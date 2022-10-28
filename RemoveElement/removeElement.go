944215b4cfab179d5573c5a8186c38624b83fe82
Siddiga Gadirova <siddiga.gadirova@technofusion.az>
2022-10-28 16:06:59 +0400

    -- add removeElement problem solution

---
+++ RemoveElement/removeElement.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(removeElement([]int{3, 2, 2, 3}, 3))
+	fmt.Println(removeElement([]int{0, 1, 2, 2, 3, 0, 4, 2}, 2))
+
+}
+
+func removeElement(nums []int, val int) int {
+	k := 0
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			nums[k] = nums[i]
+			k++
+		}
+	}
+
+	return k
+}

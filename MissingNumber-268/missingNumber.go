9b02897010fb0655a27f371f82a8c0cc84f5c525
Siddiga Gadirova <sigadi@taltech.ee>
2025-02-25 16:43:09 +0200

    268-Missing Number

---
+++ MissingNumber-268/missingNumber.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{3, 0, 1}
+	fmt.Println(missingNumber(nums)) // Output: 2
+}
+
+func missingNumber(nums []int) int {
+	n := len(nums)
+	expectedSum := n * (n + 1) / 2
+	actualSum := 0
+
+	for _, num := range nums {
+		actualSum += num
+	}
+
+	return expectedSum - actualSum
+}

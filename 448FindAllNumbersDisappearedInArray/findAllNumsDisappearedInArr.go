e56e7c8c9b8e923df2d132abe5154ef09636d49d
Siddiga Gadirova <sigadi@taltech.ee>
2025-02-25 17:17:23 +0200

    bug fixed

--- 448FindAllNumbersDisappearedInArray/findAllNumsDisappearedInArr.go
+++ 448FindAllNumbersDisappearedInArray/findAllNumsDisappearedInArr.go
@@ -5,6 +5,10 @@ import "fmt"
 func main() {
 	nums := []int{4, 3, 2, 7, 8, 2, 3, 1}
 	fmt.Println(findDisappearedNumbers(nums)) // Output: [5,6]
+
+	nums2 := []int{1, 1}
+	fmt.Println(findDisappearedNumbers(nums2)) // Output: [2]
+
 }
 
 func findDisappearedNumbers(nums []int) []int {
@@ -17,7 +21,7 @@ func findDisappearedNumbers(nums []int) []int {
 	}
 
 	//check which number is missing
-	for i := 1; i < len(nums); i++ {
+	for i := 1; i < len(nums)+1; i++ {
 		if !numMap[i] {
 			result = append(result, i)
 		}
